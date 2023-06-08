a7624a2fcc1d27b0b0bb2fd54dced6a8ec4bb57d
Shaheen Siddiqui <shanvisiddya@gmail.com>
2023-06-08 21:13:05 +0530

    initial commit

---
+++ src/frontend/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error, assumenda. Suscipit quasi 
+    maiores obcaecati vitae exercitationem quo doloribus eligendi sapiente aut blanditiis, 
+    est placeat velit illo molestias asperiores officiis ea.
+    </div>
+  );
+}
+
+export default App;
