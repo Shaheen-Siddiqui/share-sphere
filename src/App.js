52a52657b400a1aea1edc39f847d9eb5ca365cff
Shaheen Siddiqui <shanvisiddya@gmail.com>
2023-06-13 17:37:46 +0530

    made a new leftsidebar component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 const MobileHeader = lazy(() => import("./frontend/components/Header/Header"));
 const MobileFooter = lazy(() => import("./frontend/components/Footer/Footer"));
-
 const Explore = lazy(() => import("./frontend/pages/Explore/Explore"));
+const Home = lazy(() => import("./frontend/pages/Home/Home"));
+
 function App() {
   return (
     <Suspense fallback="loading...">
       <MobileHeader />
       <div style={{ marginTop: "6.7rem" }}>
+        {/* Mobile view */}
         <MobileFooter />
-        <Explore/>
+        {/* ----------- */}
+      
+        <Home />
       </div>
     </Suspense>
   );
