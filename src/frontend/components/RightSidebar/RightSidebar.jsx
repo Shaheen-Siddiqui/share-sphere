8fa7c5ccf8e350ccc7286efa3cd6744a8141a367
Shaheen Siddiqui <shanvisiddya@gmail.com>
2023-06-15 17:53:13 +0530

    fixed the position of left and right sidebar

--- src/frontend/components/RightSidebar/RightSidebar.jsx
+++ src/frontend/components/RightSidebar/RightSidebar.jsx
@@ -2,10 +2,24 @@ import "./RightSidebar.css";
 export const RightSidebar = () => {
   return (
     <div className="right-sidebar-case" id="lapy-sidebar-case">
-     {/* <strong> who to follow</strong>
-      <hr /> */}
       <div className="follow-user-case">
-        <div className="follow-user">
+        <div className="follow-user row">
+          <img
+            align="center"
+            loading="lazy"
+            src="https://banner2.cleanpng.com/20180329/zue/kisspng-computer-icons-user-profile-person-5abd85306ff7f7.0592226715223698404586.jpg"
+            alt=""
+          />
+          <p>
+            <p>John kids</p>
+            <p>@Johnkidd</p>
+          </p>
+          <button className="row">
+            <ion-icon name="add-outline"></ion-icon>
+            Follow
+          </button>
+        </div>
+        <div className="follow-user row">
           <img
             width="40rem"
             height="40rem"
@@ -18,11 +32,11 @@ export const RightSidebar = () => {
             <p>John kids</p>
             <p>@Johnkidd</p>
           </p>
-          <button>
+          <button className="row">
             <ion-icon name="add-outline"></ion-icon> Follow
           </button>
         </div>
-        <div className="follow-user">
+        <div className="follow-user row">
           <img
             width="40rem"
             height="40rem"
@@ -35,24 +49,7 @@ export const RightSidebar = () => {
             <p>John kids</p>
             <p>@Johnkidd</p>
           </p>
-          <button>
-            <ion-icon name="add-outline"></ion-icon> Follow
-          </button>
-        </div>
-        <div className="follow-user">
-          <img
-            width="40rem"
-            height="40rem"
-            align="center"
-            loading="lazy"
-            src="https://banner2.cleanpng.com/20180329/zue/kisspng-computer-icons-user-profile-person-5abd85306ff7f7.0592226715223698404586.jpg"
-            alt=""
-          />
-          <p>
-            <p>John kids</p>
-            <p>@Johnkidd</p>
-          </p>
-          <button>
+          <button className="row">
             <ion-icon name="add-outline"></ion-icon> Follow
           </button>
         </div>
--- src/frontend/pages/Home/Home.jsx
+++ src/frontend/pages/Home/Home.jsx
@@ -6,10 +6,11 @@ import { LeftSideBar } from "../../components/LeftSideBar/LeftSideBar";
 
 export const Home = () => {
   return (
-
-    <center >
+    <center>
       <div className="home-main-case">
-        <RightSidebar />
+        <div className="right-sidebar-container">
+          <RightSidebar />
+        </div>
         <main className="home-case">
           <div className="search-icon-case">
             <ion-icon
@@ -17,13 +18,81 @@ export const Home = () => {
               name="add-circle-sharp"
               id="search-add-icon"
             ></ion-icon>
-            <input
-              type="text"
-              placeholder="Write something intrasting..."
-            />
+            <input type="text" placeholder="Write something intrasting..." />
           </div>
+          
+          {/* <div className="user-post-main-case">
+            <div className="about-user">
+              <img
+                height="40"
+                width="auto"
+                src="https://imgv3.fotor.com/images/slider-image/A-clear-image-of-a-woman-wearing-red-sharpened-by-Fotors-image-sharpener.jpg"
+                alt="user profile pic"
+              />
+              <div>
+                <h1>Guest User</h1>
+                <strong>@guestuser</strong>
+                <p>2023</p>
+              </div>
+              <button>
+                <ion-icon
+                  size="small"
+                  name="ellipsis-vertical-sharp"
+                ></ion-icon>
+              </button>
+            </div>
+
+            <div className="user-post">
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non
+                culpa corporis repellat eligendi ad architecto officia sapiente
+                libero? Doloribus, dicta.
+              </p>
+              <img
+                src="https://imgv3.fotor.com/images/homepage-feature-card/make-blurry-girl-image-clear-with-Fotor-ai-image-enhancer.png"
+                alt="user posted image"
+                className="posted-main-image"
+              />
+              <div className="like-bookmark-icon">
+                <div>
+                  <ion-icon size="small" name="heart-outline"></ion-icon>
+                  <p>0</p>
+                </div>
+                <ion-icon size="small" name="bookmark-outline"></ion-icon>
+              </div>
+            </div>
+            <div className="post-comment-case">
+              <img
+                height="30"
+                width="29rem"
+                src="https://imgv3.fotor.com/images/slider-image/A-clear-image-of-a-woman-wearing-red-sharpened-by-Fotors-image-sharpener.jpg"
+                alt="user-image"
+              />
+              <div className="write-comment-case">
+                <input placeholder="Write a comment..." />
+                <button>
+                  <strong>𝖕𝖔𝖘𝖙✎ </strong>
+                </button>
+              </div>
+            </div>
+
+            <div className="about-user">
+              <img
+                height="30"
+                width="29rem"
+                src="https://static.toiimg.com/photo/6444014.cms?imgsize=25045"
+                alt="user profile pic"
+              />
+              <div>
+                <h1>Guest User</h1>
+              </div>
+            </div>
+            
+          </div> */}
         </main>
-        <LeftSideBar />
+        <div className="left-sidebar-container">
+          <LeftSideBar />
+        </div>
       </div>
     </center>
   );
