b77b1a95dee897a8d82e6e7a0519ced5801aa8e1
Shaheen Siddiqui <shanvisiddya@gmail.com>
2023-06-16 20:51:08 +0530

    feat: linked left side baar buttons to routes

--- src/frontend/components/LeftSideBar/LeftSideBar.jsx
+++ src/frontend/components/LeftSideBar/LeftSideBar.jsx
@@ -1,28 +1,43 @@
 import { Link } from "react-router-dom";
+
 //internal imports
 import "./LeftSideBar.css";
 export const LeftSideBar = () => {
   return (
     <div className="left-sidebar-case">
       <nav className="navbar-links">
-        <div className="side-item">
-          <ion-icon name="home" size="small" id="ftr-icon"></ion-icon>
-          <h2>Home</h2>
-        </div>
+        <Link to="/">
+          <div className="side-item">
+            <ion-icon name="home" size="small" id="ftr-icon"></ion-icon>
+            <h2>Home</h2>
+          </div>
+        </Link>
 
-        <div className="side-item">
-          <ion-icon name="aperture" size="small" id="ftr-icon"></ion-icon>{" "}
-          <h2>Explore</h2>
-        </div>
+        <Link to="/explore">
+          <div className="side-item">
+            <ion-icon name="aperture" size="small" id="ftr-icon"></ion-icon>{" "}
+            <h2>Explore</h2>
+          </div>
+        </Link>
 
-        <div className="side-item">
-          <ion-icon name="bookmark" size="small" id="ftr-icon"></ion-icon>{" "}
-          <h2>Bookmark</h2>
-        </div>
-        <div className="side-item">
-          <ion-icon name="person-circle" size="small" id="ftr-icon"></ion-icon>
-          <h2>Profile</h2>
-        </div>
+        <Link to="/bookmark">
+          <div className="side-item">
+            <ion-icon name="bookmark" size="small" id="ftr-icon"></ion-icon>{" "}
+            <h2>Bookmark</h2>
+          </div>
+        </Link>
+
+        <Link to="/profile">
+          <div className="side-item">
+            <ion-icon
+              name="person-circle"
+              size="small"
+              id="ftr-icon"
+            ></ion-icon>
+            <h2>Profile</h2>
+          </div>
+        </Link>
+
         <div className="side-item">
           <ion-icon size="small" name="leaf-sharp" id="ftr-icon"></ion-icon>
           <h2>new post</h2>
