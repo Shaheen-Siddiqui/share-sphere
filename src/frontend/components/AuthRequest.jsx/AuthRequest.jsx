e27beca8c5b0db5d97827a43c0aefd98d6907f8b
Shaheen Siddiqui <shanvisiddya@gmail.com>
2023-06-26 21:48:06 +0530

    7fix

--- src/frontend/components/AuthRequest.jsx/AuthRequest.jsx
+++ src/frontend/components/AuthRequest.jsx/AuthRequest.jsx
@@ -1,5 +1,5 @@
 import "./AuthRequest.css";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 export const AuthRequest = () => {
   const navigate=useNavigate()
   
