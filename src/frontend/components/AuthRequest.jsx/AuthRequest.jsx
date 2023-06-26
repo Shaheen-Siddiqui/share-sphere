f9ac0128643964ce02dd324177f58f039f4bd1b9
Shaheen Siddiqui <shanvisiddya@gmail.com>
2023-06-26 21:45:30 +0530

    6fix

--- src/frontend/components/AuthRequest.jsx/AuthRequest.jsx
+++ src/frontend/components/AuthRequest.jsx/AuthRequest.jsx
@@ -7,15 +7,15 @@ export const AuthRequest = () => {
 
     <div className="request-main-case">
       <h1>Don’t miss what’s happening</h1>
-      <Link onClick={()=>navigate("/login")}>
-        <button id="login-animation" className="menu-button">
+      {/* <Link > */}
+        <button id="login-animation" className="menu-button" onClick={()=>navigate("/login")}>
           <span></span>
           <span></span>
           <span></span>
           <span></span>
           login here
         </button>
-      </Link>
+      {/* </Link> */}
 
       <img
         src="https://images.squarespace-cdn.com/content/v1/5630dfc6e4b0303d4aea132b/1553073872212-WAQSZANTLRTH1BOI64FD/08_Movie.gif?format=500w"
