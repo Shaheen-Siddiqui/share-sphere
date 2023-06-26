568c1f03030bd050e7a92cb896c6de77b05a10fb
Shaheen Siddiqui <shanvisiddya@gmail.com>
2023-06-26 20:43:50 +0530

    fix

--- src/frontend/pages/authentication/Login.jsx
+++ src/frontend/pages/authentication/Login.jsx
@@ -11,6 +11,7 @@ import { useContext } from "react";
 import { AuthContext } from "../../hook/context/AuthContext";
 
 export const LogIn = () => {
+  console.log("Hi");
   const { loginService } = useContext(AuthContext);
   const location = useLocation();
   const [passwordIcon, setPasswordIcon] = useState(false);
